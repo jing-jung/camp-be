07155d2e88988519ff3b1788b9eb1239626e6b92
jing-jung username <yhn5729@skuniv.ac.kr>
2026-07-08 16:56:24 +0900

    fix: Fix CloudFront CookieBehavior error when caching is disabled

--- infra/terraform/modules/frontend_cloudfront_lambda/main.tf
+++ infra/terraform/modules/frontend_cloudfront_lambda/main.tf
@@ -30,7 +30,7 @@ resource "aws_cloudfront_cache_policy" "no_cache" {
 
   parameters_in_cache_key_and_forwarded_to_origin {
     cookies_config {
-      cookie_behavior = "all"
+      cookie_behavior = "none"
     }
 
     headers_config {
@@ -38,7 +38,7 @@ resource "aws_cloudfront_cache_policy" "no_cache" {
     }
 
     query_strings_config {
-      query_string_behavior = "all"
+      query_string_behavior = "none"
     }
 
     enable_accept_encoding_gzip   = true
